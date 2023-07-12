a9d8afc98e38957867f5807b042f7585e915399b
IvanJhon <110226186+IvanJhon@users.noreply.github.com>
2023-07-12 18:29:38 +0800

    Update php.php

--- php.php
+++ php.php
@@ -1,20 +1,38 @@
 
+
+<?php
 use Exception;
 use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
-
-$uri = mongodb+srv://ivancarito2002:Carito2020@cluster0.qpf78xw.mongodb.net/?retryWrites=true&w=majority;
-
+// Connect to the database
+$conn = new MongoClient("mongodb+srv://ivancarito2002:Carito2020@cluster0.qpf78xw.mongodb.net/IMS?retryWrites=true&w=majority");
+$db = $conn->IMS;
+$collection = $db->user;
 // Specify Stable API version 1
 $apiVersion = new ServerApi(ServerApi::V1);
 
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
-
-try {
-    // Send a ping to confirm a successful connection
-    $client->selectDatabase('admin')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    printf($e->getMessage());
+// Check if the user is logged in
+if (isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit;
 }
+// Check if the form is submitted
+if (isset($_POST['user']) && isset($_POST['pass'])) {
+    // Get the username and password from the form
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    // Check if the username and password are valid
+    $user = $collection->findOne(array('user' => $username, 'pass' => $password));
+    if ($user) {
+        // The user is logged in
+        $_SESSION['user'] = $username;
+        header("Location: index.php");
+        exit;
+    } else {
+        // The username and password are not valid
+        echo "Invalid username or password.";
+    }
+}
+?>
