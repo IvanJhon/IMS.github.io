7eef13e60c15e6a01afced67d60fe3b946c052b1
IvanJhon <110226186+IvanJhon@users.noreply.github.com>
2023-07-12 18:06:25 +0800

    Update php.php

--- php.php
+++ php.php
@@ -1,32 +1,28 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-// Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://ivancarito2002:Carito2020@cluster0.qpf78xw.mongodb.net/IMS?retryWrites=true&w=majority");
-// Create a user model
-const User = mongoose.model("user", {
-  username: String,
-  password: String,
-});
-// Handle the login form submission
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  // Find the user in the database
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return res.status(500).send("Error finding user");
+<?php
+// Connect to the database
+$conn = new MongoClient("mongodb://localhost:27017");
+$db = $conn->mydb;
+$collection = $db->users;
+// Check if the user is logged in
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit;
+}
+// Check if the form is submitted
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    // Get the username and password from the form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // Check if the username and password are valid
+    $user = $collection->findOne(array('username' => $username, 'password' => $password));
+    if ($user) {
+        // The user is logged in
+        $_SESSION['username'] = $username;
+        header("Location: index.php");
+        exit;
+    } else {
+        // The username and password are not valid
+        echo "Invalid username or password.";
     }
-    if (!user) {
-      return res.status(401).send("Invalid username or password");
-    }
-    // Check the password
-    if (user.password !== password) {
-      return res.status(401).send("Invalid username or password");
-    }
-    // Login the user
-    req.session.user = user;
-    res.redirect("/");
-  });
-});
-// Start the server
-app.listen(3000, () => console.log("Server started on port 3000"));
+}
+?>
